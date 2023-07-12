0ca0d26986cb2fa8222f13b3a7f3fdb8fbf85f9b
piatachenko <122259487+piatachenko@users.noreply.github.com>
2023-07-12 12:00:00 +0200

    add animation

--- src/components/HoverBlock.tsx
+++ src/components/HoverBlock.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import { useRef, type MouseEvent } from "react";
 
 export default function HoverBlock() {
+  const ref = useRef<HTMLDivElement>(null);
+  function handleMouseOver(e: MouseEvent<HTMLDivElement>) {
+    const el = e.target as HTMLDivElement;
+    el.classList.add("active");
+    setTimeout(() => {
+      el.classList.remove("active");
+    }, 400);
+  }
+
   return (
-    <></>
-  )
+    <>
+      <div
+        ref={ref}
+        onMouseOver={handleMouseOver}
+        className="aspect-square w-[10vw] bg-white opacity-0 transition-all duration-[.4s] ease-[cubic-bezier(.645,.045,.355,1)] sm:w-14"
+      />
+    </>
+  );
 }
